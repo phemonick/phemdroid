eb3bc84903d91315537951d5058f54e8dc5e7b27
Oluwafemi Adekunle <phemy.smith@gmail.com>
2020-04-20 17:28:56 +0100

    reverse strings

---
+++ Javascript/Code_challenge/reverse.js
@@ -0,0 +1,22 @@
+function reverse(word){
+  let newWord = []
+  for(let i = word.length -1; i >=0; i--) {
+    newWord.push(word[i]);
+    
+  }
+  console.log(newWord)
+  // add whatever parameters you deem necessary - good luck!
+}
+
+function reverse2(word){
+  let newWord = []
+  for(let i = word.length -1; i >=0; i--) {
+    newWord.push(word[i]);
+    
+  }
+  console.log(newWord)
+  // add whatever parameters you deem necessary - good luck!
+}
+
+reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
